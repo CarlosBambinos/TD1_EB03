34140525a73c44ab315312146b120900d00edea9
Carlos_Bambinos <5zigen20@gmail.com>
2023-11-23 15:20:40 +0100

    définition constantes et class

---
+++ app/src/main/java/com/example/td1_eb03/Slider.java
@@ -0,0 +1,28 @@
+package com.example.td1_eb03;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Slider extends View {
+
+    private float currentValue , minSlider, maxSlide ;
+    private float cursorDiameter, barWidth, barLength;
+
+    private int cursorColor, valueColor, barColor;
+
+    public final static float DEFAULT_BAR_LENGHT = 160;
+    public final static float DEFAULT_CURSOR_DIAMETER = 40;
+    public final static float DEFAULT_BAR_WIDTH = 30;
+
+    public final static int DEFAULT_CURSOR_COLOR = 15;
+    public final static int DEFAULT_VALUE_COLOR = 10;
+    public final static int DEFAULT_BAR_COLOR = 20;
+
+    public Slider(Context context){
+
+    };
+    public Slider(Context context, AttributeSet attrs){
+
+    };
+}
