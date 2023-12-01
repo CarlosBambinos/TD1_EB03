cc8e6a81005413f2723ec2d61bf7e349b6242437
Carlos_Bambinos <5zigen20@gmail.com>
2023-12-01 10:03:24 +0100

    curseur

--- app/src/main/java/com/example/td1_eb03/MainActivity.java
+++ app/src/main/java/com/example/td1_eb03/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.td1_eb03;
 
+import androidx.annotation.MenuRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mtv;
     Slider msl;
+
+
+    int MenuRes = R.menu.menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
         msl = findViewById(R.id.slider3);
 
         msl.setSliderChangeListener((value) ->  mtv.setText(String.format("%2f",value)));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(MenuRes, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
 
